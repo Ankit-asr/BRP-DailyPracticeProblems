49871f015a3d54dc28d3ffb9bddc2e33a9854ed3
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-10-08 16:05:41 +0530

    [Ankit]Add : Nibbbles Decimal to Binary Conversion

---
+++ BRP_DailyPracticeProblems/ExtendDecimalToBinary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BRP_DailyPracticeProblems
+{
+    class ExtendDecimalToBinary
+    {
+        public int SwapNibbles(int x)
+        {
+            return ((x & 0x0F) << 4 |
+                    (x & 0xF0) >> 4);
+        }
+        public void Nibble()
+        {
+            Console.WriteLine("Enter a Decimal Number for Nibble");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Swapped Nibble is :" + SwapNibbles(x));
+        }
+    }
+}
+
+
--- BRP_DailyPracticeProblems/Program.cs
+++ BRP_DailyPracticeProblems/Program.cs
@@ -9,6 +9,9 @@ namespace BRP_DailyPracticeProblems
             Console.WriteLine("Hello Welcome to BRP Daily Practice Problems");
             DecimalToBinary obj1 = new DecimalToBinary();
             obj1.Conversion();
+            Console.WriteLine();
+            ExtendDecimalToBinary obj2 = new ExtendDecimalToBinary();
+            obj2.Nibble();
         }
     }
 }
