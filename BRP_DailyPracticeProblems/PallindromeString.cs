8b087c89c88836836f1168bc5c905c5ac2c1bfb3
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-10-09 01:10:09 +0530

    [Ankit]Add : Check String is Anagram and Pallindrome

---
+++ BRP_DailyPracticeProblems/PallindromeString.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BRP_DailyPracticeProblems
+{
+    class PallindromeString
+    {
+        public bool IsPalindrome(string text)
+        {
+            if (text.Length <= 1)
+                return true;
+            else
+            {
+                if (text[0] != text[text.Length - 1])
+                    return false;
+                else
+                    return IsPalindrome(text.Substring(1, text.Length - 2));
+            }
+        }
+        public void CheckPallindrome()
+        {
+            string str1;
+            bool tf;
+            Console.Write(" Input a string : ");
+            str1 = Console.ReadLine();
+            tf = IsPalindrome(str1);
+            if (tf == true)
+            {
+                Console.WriteLine("The string is Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("The string is not a Palindrome");
+            }
+        }
+    }
+}
--- BRP_DailyPracticeProblems/Program.cs
+++ BRP_DailyPracticeProblems/Program.cs
@@ -7,11 +7,15 @@ namespace BRP_DailyPracticeProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Welcome to BRP Daily Practice Problems");
-            DecimalToBinary obj1 = new DecimalToBinary();
-            obj1.Conversion();
-            Console.WriteLine();
-            ExtendDecimalToBinary obj2 = new ExtendDecimalToBinary();
-            obj2.Nibble();
+            //DecimalToBinary obj1 = new DecimalToBinary();
+            //obj1.Conversion();
+            //Console.WriteLine();
+            //ExtendDecimalToBinary obj2 = new ExtendDecimalToBinary();
+            //obj2.Nibble();
+            //Anagram obj3 = new Anagram();
+           // obj3.AnagramCheck();
+            PallindromeString obj4 = new PallindromeString();
+            obj4.CheckPallindrome();
         }
     }
 }
