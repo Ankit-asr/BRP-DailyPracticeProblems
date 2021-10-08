328b507c9a544385712577cc3012f2a6faf8b7ff
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-10-08 15:17:50 +0530

    [Ankit]Add : Welcome Message

---
+++ BRP_DailyPracticeProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BRP_DailyPracticeProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Welcome to BRP Daily Practice Problems");
+        }
+    }
+}
